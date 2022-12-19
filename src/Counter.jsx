a10b23d48adb0963acac7b44aa505ea49b2d871c
Chrsteffen <Chrsteffen1@gmail.com>
2022-12-19 17:20:28 -0500

    add setcount functionality

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useState } from 'react';
 
 const Counter = () => {
+  const [count, setCount] = useState(0)
+
+
   return (  
     <>
-    <span>Current Count: 0</span>
+    <span>Current Count: {count} </span>
     <section>
-      <button>+</button>
-      <button>-</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </section>
     </>
   );
