61239cdd7bcdd9325fe4b5046690f8e7c67b460a
Chrsteffen <Chrsteffen1@gmail.com>
2022-12-19 17:02:11 -0500

    set up page

---
+++ src/Counter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Counter = () => {
+  return (  
+    <>
+    <span>Current Count: 0</span>
+    <section>
+      <button>+</button>
+      <button>-</button>
+    </section>
+    </>
+  );
+}
+ 
+export default Counter;
