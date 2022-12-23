51fcf26aa36243123b291da3143a03b966fad450
Chrsteffen <Chrsteffen1@gmail.com>
2022-12-23 16:03:30 -0500

    add color state

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,27 +4,32 @@ import { useState } from 'react';
 const Counter = () => {
   const [count, setCount] = useState(0)
   const [resetCount, setResetCount] = useState(0);
+  const [color, setColor] = useState('black')
 
   const handleIncrement = () => {
     setCount(count + 1);
+    setColor('lightgreen')
   };
   
   const handleDecrement = () => {
     setCount(count - 1);
+    setColor('pink')
   };
 
   const handleMultiply = () =>{
     setCount(count * count)
+    setColor('blue')
   }
 
   const handleReset = () => {
     setCount(0);
     setResetCount(resetCount + 1);
+    setColor('black')
   };
 
   return (  
     <>
-    <span>Current Count: {count}</span> 
+    <span style={{color: color}}>Current Count: {count}</span> 
     <span>Reset Count: {resetCount}</span>
     <section>
       <button onClick={handleIncrement}>+</button>
