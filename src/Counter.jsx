fdee66c4a1ef0903c39d7c0fc4cd4423b9e84f9c
Chrsteffen <Chrsteffen1@gmail.com>
2022-12-23 16:46:52 -0500

    start calculator set up

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,10 +1,11 @@
+import { getValue } from "@testing-library/user-event/dist/utils";
 import React from "react";
 import { useState } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0)
   const [resetCount, setResetCount] = useState(0);
-  const [color, setColor] = useState('black')
+  const [color, setColor] = useState('white')
 
   const handleIncrement = () => {
     setCount(count + 1);
@@ -18,24 +19,40 @@ const Counter = () => {
 
   const handleMultiply = () =>{
     setCount(count * count)
-    setColor('blue')
+    setColor('lightblue')
   }
 
   const handleReset = () => {
     setCount(0);
     setResetCount(resetCount + 1);
-    setColor('black')
+    setColor('white')
   };
+
+  const handleNumber = (e) => {
+    setCount(e.target.innerHTML)
+  }
 
   return (  
     <>
-    <span style={{color: color}}>Current Count: {count}</span> 
+    <span style={{background: color}}>Current Count: {count}</span> 
     <span>Reset Count: {resetCount}</span>
     <section>
+      <button onClick={handleNumber} >1</button>
+      <button onClick={handleNumber}>2</button>
+      <button onClick={handleNumber}>3</button>
       <button onClick={handleIncrement}>+</button>
+      <button onClick={handleNumber}>4</button>
+      <button onClick={handleNumber}>5</button>
+      <button onClick={handleNumber}>6</button>
       <button onClick={handleDecrement}>-</button>
+      <button onClick={handleNumber}>7</button>
+      <button onClick={handleNumber}>8</button>
+      <button onClick={handleNumber}>9</button>
       <button onClick={handleMultiply}>*</button>
       <button onClick={handleReset}>Reset</button>
+      <button onClick={handleNumber}>0</button>
+      <button >Enter</button>
+      <button >/</button>
     </section>
     </>
   );
